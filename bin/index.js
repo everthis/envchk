6cdcec886f1a15f4f81eaf9b852e34a9d35da33f
everthis <wwwaap@gmail.com>
2017-08-09 21:16:07 +0800

    first commit

---
+++ bin/index.js
@@ -0,0 +1,7 @@
+#!/usr/bin/env node
+/*
+ * search all environment variables used in this process.pwd()
+ * support: ENV['env1'] or ENV.env2
+ */
+require('../index')
+
---
+++ test/index.js
@@ -0,0 +1,5 @@
+const var1 = ENV['ENV1']
+let var2 = ''
+var2 = ENV['env2']
+console.log(ENV.env5)
+console.log(ti.ENV.env4)
